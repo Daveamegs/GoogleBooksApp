ebec9240e0932c2188f2ee32429a58d3c123b75f
David Koffi Amegayibor <Amegah40@gmail.com>
2020-06-26 02:36:33 +0000

    Initial commit

---
+++ app/src/main/java/com/daveamegs/googlebooksapp/MainActivity.java
@@ -0,0 +1,69 @@
+package com.daveamegs.googlebooksapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+    private ProgressBar mLoadingProgress;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mLoadingProgress = (ProgressBar) findViewById(R.id.pb_loading);
+
+        try {
+            URL bookUrl = ApiUtil.buildUrl("cooking");
+            new BooksQueryTask().execute(bookUrl);
+
+        }
+        catch (Exception e) {
+            Log.d("error", e.getMessage());
+        }
+    }
+    public class BooksQueryTask extends AsyncTask<URL, Void, String> {
+
+        @Override
+        protected String doInBackground(URL... urls) {
+            URL searchUrl = urls[0];
+            String result = null;
+            try {
+                result = ApiUtil.getJson(searchUrl);
+            }catch (IOException e){
+                Log.e("Error", e.getMessage());
+            }
+            return result;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            mLoadingProgress.setVisibility(View.INVISIBLE);
+            TextView tvResult = (TextView) findViewById(R.id.tvResponse);
+            TextView tvError = (TextView) findViewById(R.id.tv_error);
+            tvResult.setText(result);
+
+            if (result == null){
+                tvResult.setVisibility(View.INVISIBLE);
+                tvError.setVisibility(View.VISIBLE);
+            }else {
+                tvResult.setVisibility(View.VISIBLE);
+                tvError.setVisibility(View.INVISIBLE);
+            }
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            mLoadingProgress.setVisibility(View.VISIBLE);
+        }
+    }
+}
